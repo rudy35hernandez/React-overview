52f9a51f457ceee7951bbca115409e4f176f1658
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-20 00:19:40 -0800

    added some js to the html in react

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
-  return ( <div className="expense-item">
-      <div>March 28, 2021</div>;
-      <div className="expense-item__description">
-          <h2>Car Insurance</h2>
-          <div className="expense-item__price">$249.67</div>
+
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle= 'Car Insurance';
+  const expenseAmount = 294.67
+
+
+  return ( <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+          <h2>{expenseTitle}</h2>
+          <div className='expense-item__price'>$ {expenseAmount}</div>
       </div>
   </div>
 
