1c41cff185960d800beba5ff0fdb3c27a0db75c7
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-20 23:49:35 -0800

    added another component for date box and added component to the ExpenseItem component

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,23 @@
+
+import './ExpenseDate.css'
+
+function ExpenseDate(props){
+
+    ////Here we are creating variables for these props so it looks cleaner below.
+    ////Props gets added as a parameter in the function. The props then grabs the key in expenses object from app.js
+    //// toLocalString is a built in JS function, parameter is language, then how you want the date displayed
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
+
+    return (
+
+        <div className="expense-date">
+          <div className="expense-date__month">{month}</div>
+          <div className="expense-date__day">{day}</div>
+          <div className="expense-date__year">{year}</div>
+      </div>
+    )
+}
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,31 +1,22 @@
+//// Import ExpenseDate component
+import ExpenseDate from "./ExpenseDate";
 //// Here we are importing our css file
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  //// Notice, we still use props but this time title because we're grabbing the title key from expenses
+  const title = props.title;
+  const amount = props.amount;
 
-    ////Here we are creating variables for these props so it looks cleaner below.
-    ////Props gets added as a parameter in the function. The props then grabs the key in expenses object from app.js
-    //// toLocalString is a built in JS function, parameter is language, then how you want the date displayed
-    const month = props.date.toLocaleString('en-US', {month: 'long'});
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = props.date.getFullYear();
-    //// Notice, we still use props but this time title because we're grabbing the title key from expenses
-    const title = props.title;
-    const amount = props.amount
-
-  return ( <div className='expense-item'>
-      <div>
-          <div>{month}</div>
-          <div>{day}</div>
-          <div>{year}</div>
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">$ {amount}</div>
       </div>
-      <div className='expense-item__description'>
-          <h2>{title}</h2>
-          <div className='expense-item__price'>$ {amount}</div>
-      </div>
-  </div>
-
-  ) 
+    </div>
+  );
 }
 
 export default ExpenseItem;
