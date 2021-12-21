3b2c5d1ba4a5491c9238a449ea4d9d15cd45c7fb
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-20 23:16:41 -0800

    added a lot more notes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,11 +4,14 @@ import './ExpenseItem.css'
 function ExpenseItem(props) {
 
     ////Here we are creating variables for these props so it looks cleaner below.
-    ////Props gets added as a parameter in the function. The props then grabs the date 
-
+    ////Props gets added as a parameter in the function. The props then grabs the key in expenses object from app.js
+    //// toLocalString is a built in JS function, parameter is language, then how you want the date displayed
     const month = props.date.toLocaleString('en-US', {month: 'long'});
     const day = props.date.toLocaleString('en-US', {day: '2-digit'});
     const year = props.date.getFullYear();
+    //// Notice, we still use props but this time title because we're grabbing the title key from expenses
+    const title = props.title;
+    const amount = props.amount
 
   return ( <div className='expense-item'>
       <div>
@@ -17,8 +20,8 @@ function ExpenseItem(props) {
           <div>{year}</div>
       </div>
       <div className='expense-item__description'>
-          <h2>{props.title}</h2>
-          <div className='expense-item__price'>$ {props.amount}</div>
+          <h2>{title}</h2>
+          <div className='expense-item__price'>$ {amount}</div>
       </div>
   </div>
 
