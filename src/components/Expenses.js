740e13891877c4afa9ce5d11b0d0af773ac784ab
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-21 17:24:20 -0800

    fixed my expenses react folder

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,37 +1,31 @@
-import '.Expenses.css'
-import '.ExpenseItem.js'
-import ExpenseItem from './ExpenseItem';
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
 
-function Expenses(props){
-    const expenses = [
-        {
-          id: "e1",
-          title: "Toilet Paper",
-          amount: 94.12,
-          date: new Date(2020, 7, 14),
-        },
-        { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-        {
-          id: "e3",
-          title: "Car Insurance",
-          amount: 294.67,
-          date: new Date(2021, 2, 28),
-        },
-        {
-          id: "e4",
-          title: "New Desk (Wooden)",
-          amount: 450,
-          date: new Date(2021, 5, 12),
-        },
-      ];
-
-      <div className=".expenses">
-          <ExpenseItem />
-          <ExpenseItem />
-          <ExpenseItem />
-          <ExpenseItem />
-      </div>
-
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </div>
+  );
 }
 
-export default Expenses
+export default Expenses;
