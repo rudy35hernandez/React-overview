0866177316a1f5382614965d343047926f721746
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-26 01:29:44 -0800

    creating first react hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,22 @@
-import React from "react"
+import React, { useState } from "react";
 
 //// Import ExpenseDate component
 import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card"
+import Card from "../UI/Card";
 //// Here we are importing our css file
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  /////// A react hook //////////
+  useState(props.title);
+
   //// Notice, we still use props but this time title because we're grabbing the title key from expenses
-  const title = props.title;
+  let title = props.title;
   const amount = props.amount;
+  const clickHandler = () => {
+    title = "Updated";
+    console.log(title);
+  };
 
   return (
     <Card className="expense-item">
@@ -18,8 +25,9 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className="expense-item__price">$ {amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
