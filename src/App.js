c65bf1eaa6af7b980dbc0efbad1848a4e5b8c954
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-27 23:00:38 -0800

    added newexpense component to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from "react"
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense"
 
 const App = () => {
   const expenses = [
@@ -25,7 +27,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Let's get started!</h1>
+      <NewExpense />
       <Expenses items={expenses}
       />
     </div>
