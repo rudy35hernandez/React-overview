0e32a8744192a3a871c4c847beb09f9928f04bcb
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-01-09 00:18:33 -0800

    added notes again

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -5,11 +5,9 @@ import './NewExpense.css'
 
 /// We can only pass props from parent to child, so here, we are passing the form data in the 
 /// object which we named expenseData. 
-/// onSaveExpenseData, this is the prop we're creating, a function that will eventually be triggered when something
-/// happens in this component, in this case, when the user saves
 
 const NewExpense = (props) => {
-
+  
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -17,7 +15,11 @@ const NewExpense = (props) => {
     }
     props.onAddExpense(expenseData)
   }
-
+  
+  /// onSaveExpenseData, this is the prop we're creating, a function that will eventually be triggered when something
+  /// happens in this component. It is tied to the submitHandler function in the expenseForm file where the 
+  /// user input form is saved!
+  
   return (
     <div className="new-expense">
      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
